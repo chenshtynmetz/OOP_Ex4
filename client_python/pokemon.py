8662d9359c478ea32783a2fa0b3cdb92f819ca26
tavorlevine <80433297+tavorlevine@users.noreply.github.com>
2022-01-04 13:29:45 +0200

    4/1 13:29

--- client_python/pokemon.py
+++ client_python/pokemon.py
@@ -1,8 +1,19 @@
-#
-# class Pokemon:
-#
-#     def __init__(self, jsonFile):
-#         self.
+class Pokemon:
+
+    def __init__(self, value: int, type: int, pos: tuple = None):
+        self.value = value
+        self.type = type
+        self.pos = pos
+        self.edge = pos  # cheak from pos the right edge
+        self.assign = False  # if we draw the pokimon on the graph
 
 
+class agent:
 
+    def __init__(self, _id: int, value: float, src: int, dest: int, speed: float, pos: tuple = None):
+        self.id = _id
+        self.value = value
+        self.src = src
+        self.dest = dest
+        self.speed = speed
+        self.pos = pos
