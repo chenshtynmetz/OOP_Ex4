7c0ecd509f99589d27880067b2023d42e34fbb9f
tavorlevine <80433297+tavorlevine@users.noreply.github.com>
2022-01-09 23:13:46 +0200

    9\1 23:13

--- client_python/test_gui_function.py
+++ client_python/test_gui_function.py
@@ -38,4 +38,16 @@ class TestFunctions(TestCase):
         self.assertEqual(0, e1.src)
         self.assertEqual(1, e1.dest)
 
-    # def test_allocate_agent_to_pok(self):
+    def test_allocate_agent_to_pok(self):
+        algo = GraphAlgo()
+        graph = algo.get_graph()
+        p1 = Pokemon(5, 1, (35.18753053591606, 32.10378225882353, 0.0))
+        p2 = Pokemon(8, -1, (35.2, 32.33, 0))
+        graph.add_node(0, (35.18753053591606, 32.10378225882353, 0.0))
+        graph.add_node(1, (35.18958953510896, 32.10785303529412, 0.0))
+        graph.add_edge(0, 1, 2.5)
+        e = Edge(0, 1, 2.5)
+        f = Functions(0, 0, 0, 0, 0, graph, None, None, algo)
+        path = [1, 2, 3]
+        path2 = [1, 2, 3]
+        self.assertEqual(path2, path)
